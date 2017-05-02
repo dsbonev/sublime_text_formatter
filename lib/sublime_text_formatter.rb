8f26d4a0d24b66a602587c7faf9613ebad07f3c2
Dimitar Bonev <dsbonev@gmail.com>
2017-05-02 09:22:40 +0300

    Use `file` protocol instead of `subl` one

--- lib/sublime_text_formatter.rb
+++ lib/sublime_text_formatter.rb
@@ -2,7 +2,7 @@ require 'rspec/core/formatters/base_text_formatter'
 
 class SublimeTextFormatter < RSpec::Core::Formatters::BaseTextFormatter
   def dump_backtrace(example)
-    format_backtrace(example.execution_result[:exception].backtrace, example).each do |backtrace_info|
+    format_backtrace(example.execution_result.exception.backtrace, example).each do |backtrace_info|
       output.puts detail_color("#{long_padding}# #{sublime backtrace_info}")
     end
   end
@@ -12,9 +12,9 @@ class SublimeTextFormatter < RSpec::Core::Formatters::BaseTextFormatter
     path = if file_path.starts_with?('/')
       file_path
     else
-      File.join(File.expand_path(ENV['SUBLIME_PROJECT_DIR']), file_path)
+      File.join(File.expand_path('/home/dsbonev/projects/onetap-backend'), file_path)
     end
-    "subl://open?url=file://" + path
+    "file://" + path.gsub('./', '')
   end
 
   def extract_path_from_backtrace(backtrace_info)
